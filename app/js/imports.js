eddc6a600c5238f90af0761f9847377e85509871
chakradhar-s <venkatachakradhar@gmail.com>
2017-12-13 12:07:11 +0530

    added css file as per review comments

--- app/js/imports.js
+++ app/js/imports.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap/dist/css/bootstrap.css";
-import "../scss/style.scss";
+import "../css/style.css";
 import "font-awesome/css/font-awesome.css";
 import "jquery";
